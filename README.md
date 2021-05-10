2ed537c04469c7d818deb316baba26ac44a04ca1
naranjito72 <theclick36@gmail.com>
2021-05-10 21:47:15 +0200

    clean code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# Calendar" 
+# Calendar
+
+### Manipulaci&oacute;n del Dom
+
+Creaci&oacute;n de un calendario. Utilizaci&oacute;n de un *for loop* para la creación de los elementos en el HTML
+
+[**NODES**](https://www.w3schools.com/js/js_htmldom_nodes.asp)
--- js/functions.js
+++ js/functions.js
@@ -6,12 +6,8 @@ let mesActual =  hoy.getMonth();
 let anyActual = hoy.getFullYear();
 // llamada al año a coger en html
 let viewYear = document.getElementById("year");
-// viewYear.value = anyActual;
-// let agafarAny = viewYear.value;
 // mismo con los meses
 let viewMonth = document.getElementById("month");
- //viewMonth.value = mesActual;
-// let agafarMes = viewMonth.value;
 //array de meses
 let months = [
     'Enero',
@@ -27,60 +23,33 @@ let months = [
     'Noviembre',
     'Diciembre'
 ];
-//array dias
-// let listaDias = [
-//     'Lunes',
-//     'Martes',
-//     'Miércoles',
-//     'Jueves',
-//     'Viernes',
-//     'Sábado',
-//     'Domingo',
-// ];
-//llamada a los elementos del html
 
+//llamada a los elementos del html
 let mesIAny = document.getElementById("mesIAny");
 mostarCalendario(mesActual, anyActual);
-// you don't
+
 function previous(){
     anyActual = (mesActual == 0) ? anyActual - 1 : anyActual;
     mesActual = (mesActual == 0) ? 11 : mesActual - 1;
     mostarCalendario(mesActual, anyActual);
-    // if(mesActual == 0) {
-    //   anyActual = anyActual - 1;
-    //   mesActual = 11;
-    // } else {
-    //   mesActual = mesActual - 1;
-    // }
-    // mostarCalendario(mesActual, anyActual);
 }
-// you don't
-    function next(){
+
+function next(){
        anyActual = (mesActual == 11) ? anyActual + 1 : anyActual;
        mesActual = (mesActual + 1) % 12;
       mostarCalendario(mesActual, anyActual);
-    // if (mesActual == 11) {
-    //   anyActual = anyActual + 1;
-    //   mesActual = 0;
-    // } else {
-    //     mesActual = mesActual + 1;
-    // }
-    // mostarCalendario(mesActual, anyActual);
-
     }
-// you don't
-    function jumpMonth(){
-      //  anyActual = parseInt(agafarAny);
-      //  mesActual = parseInt(agafarMes);
+
+function jumpMonth(){
         mesActual = parseInt(viewMonth.value);
         mostarCalendario(mesActual, anyActual);
     }
-    function jumpYear() {
-      // anyActual = parseInt(agafarAny);
+
+function jumpYear() {
       anyActual = parseInt(viewYear.value);
         mostarCalendario(mesActual, anyActual);
     }
-// you work
+
 function mostarCalendario(month, year){
     let primerDia =  new Date(year, month).getDay() - 1; // it begun on sunday in english, thus -1 to start on monday
     let diasDelMes = 32 - new Date(year, month, 32).getDate();
@@ -88,14 +57,9 @@ function mostarCalendario(month, year){
     let tbl = document.getElementById("calendar-body");
 
     //qué escribimos en el
-
     tbl.innerHTML = "";
     //
     mesIAny.innerHTML = months[month] + " " + year;
-    // agafarAny.value = year;
-    // agafarMes.value = month;
-    //anyActual = year;
-  //  mesActual = month;
 //celdas
     // desde donde comenzar el mes
     let date = 1;
